399f5e719f6035ca079ac039e6b702f0d03f39e8
Rodrigo Castro <82513713+RodBC@users.noreply.github.com>
2021-10-06 09:11:25 -0300

    Create emails.py

---
+++ emails.py
@@ -0,0 +1,25 @@
+#automatically send email with report
+
+import email
+import mimetypes
+import smtplib
+import os
+
+
+def generate_error_email(sender, recipient, subject, body):
+    """Creates an email without an attachement."""
+    # Basic Email formatting
+    message = email.message.EmailMessage()
+    message["From"] = sender
+    message["To"] = recipient
+    message["Subject"] = subject
+    message.set_content(body)
+
+    return message
+
+
+def send_email(message):
+    """Sends the message to the configured SMTP server."""
+    mail_server = smtplib.SMTP('localhost')
+    mail_server.send_message(message)
+    mail_server.quit()
