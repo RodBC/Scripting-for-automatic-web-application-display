cfae094be8c84617f3a06793644fcf54a44a6e8e
Rodrigo Castro <82513713+RodBC@users.noreply.github.com>
2021-10-06 09:21:15 -0300

    Create supplier_image_upload.py

---
+++ supplier_image_upload.py
@@ -0,0 +1,20 @@
+# This example shows how a file can be uploaded using
+# The Python Requests module
+
+import requests, os
+
+#Declare variables to be used
+url = "http://localhost/upload/"
+user = os.getenv('USER')
+image_directory = '/home/{}/supplier-data/images/'.format(user)
+files = os.listdir(image_directory)
+
+# Parsing through all the images
+for image_name in files:
+    # Accepting files that has jpeg extension and ignoring hidden files
+    if not image_name.startswith('.') and 'jpeg' in image_name:
+        # creating absolute path for each image
+        image_path = image_directory + image_name
+        # uploading jpeg files
+        with open(image_path, 'rb') as opened:
+            r = requests.post(url, files={'file': opened})
